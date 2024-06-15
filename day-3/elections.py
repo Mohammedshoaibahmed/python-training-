7722a9f335244ab78f030d1f5f58bafc89f26afe
Mohammed Shoaib ahmed <mohammedshoaib4042@gmail.com>
2024-06-15 10:45:50 +0530

    day-3

---
+++ day-3/elections.py
@@ -0,0 +1,19 @@
+list=[2,3,4,4,5,4,5] 
+dict={}
+l=len(list)  
+vote=int(input("cast u r vote"))    
+list.append(vote)
+for i in list:
+    if i in dict:
+        dict[i]+=1
+    else:
+        dict[i]=1
+maximum=0
+key=0         
+for i,j in dict.items():
+    if j>maximum:       
+        maximum=j
+        key=i    
+for i,j in dict.items():
+    if j==maximum:
+        print(i)  
